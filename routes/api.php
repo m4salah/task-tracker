e66e973e75a50dc47b49f1dfa0916319b677010b
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2022-08-25 04:43:40 +0200

    auth api url

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+    Route::get('/', function() {
+        return responseJson(1, 'Task-Manager is running');
+    });
+    Route::prefix('user')->group(function () {
+        Route::post('register', 'AuthController@register');
+        Route::post('login', 'AuthController@login')->name('login');
+    });
 });
+
