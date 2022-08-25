a5c95c9b62ebf9dbcbed49e1955015d4020b51f7
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2022-08-25 04:42:15 +0200

    configure auth

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,9 +2,10 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
-
+use Illuminate\Support\Arr;
 class Handler extends ExceptionHandler
 {
     /**
@@ -46,5 +47,14 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            responseJson(1, 'authorized', 'hello');
+            return response()->json(['error' => 'Unauthenticated.'], 401);
+        }
+        return redirect()->guest(route($login));
     }
 }
