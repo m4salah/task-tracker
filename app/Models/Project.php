173fb236f47a7a6c40081f78bec56eadd11f2dd2
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2022-08-26 07:43:33 +0200

    project and task model

---
+++ app/Models/Project.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+    ];
+
+    public function tasks() {
+        return $this->hasMany(Task::class);
+    }
+
+    public function creator() {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+}
