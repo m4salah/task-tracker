5c63f68972db2218736da688a73d8033955adedb
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2022-08-26 07:46:09 +0200

    one to many projects

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function projects() {
+        return $this->hasMany(Project::class, 'created_by')->select(['id', 'title', 'description']);
+    }
 }
