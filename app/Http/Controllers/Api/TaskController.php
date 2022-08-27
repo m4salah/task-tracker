faee944032e562e27be25596c9b23c8c8f103623
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2022-08-28 01:58:32 +0200

    submit endpoint beside refactoring

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -17,11 +17,14 @@ class TaskController extends Controller
         ]);
         $user = auth()->user();
         $project = Project::find($request->project_id);
-        if($project && $user && $user->is_admin && $user->id == $project->created_by) {
+        if(!$project) {
+            return responseJson(0, 'Project not found');
+        }
+        if($user && $user->is_admin && $user->id == $project->created_by) {
             $tasks = $project->tasks;
             return responseJson(1, 'All tasks', $tasks);
         }
-        return responseJson(0, 'User not authorized or project not found');
+        return responseJson(0, 'User not authorized');
     }
 
     public function create(Request $request) {
@@ -32,7 +35,10 @@ class TaskController extends Controller
             'assigned_to' => 'required',
         ]);
         $user = auth()->user();
-        $project = Project::findOrFail($request->project_id);
+        $project = Project::find($request->project_id);
+        if(!$project) {
+            return responseJson(0, 'Project not found');
+        }
         if($user && $user->is_admin && $user->id == $project->created_by) {
             $project->tasks()->create($request->all());
             return responseJson(1, 'Task created succesfully');
@@ -42,7 +48,7 @@ class TaskController extends Controller
 
     public function userTasks(Request $request) {
         $user = auth()->user();
-        $project = Project::findOrFail($request->project_id);
+        $project = Project::find($request->project_id);
         if($user && !$user->is_admin) {
             $tasks = Task::where('assigned_to', $user->id)->get();
             return responseJson(1, 'All tasks', $tasks);
@@ -53,28 +59,46 @@ class TaskController extends Controller
 
     public function update($id, Request $request)
     {
-        $task = Task::findOrFail($id);
-        $project = $task->project;
+        $task = Task::find($id);
         $user = auth()->user();
-        if ($task && $user && $user->is_admin && $user->id == $project->created_by){
-            if($request->has('title') && $request->title != ''){
-                $task->update([
-                    'title' => request('title'),
-                ]);
-            }
-            if($request->has('description') && $request->description != ''){
-                $task->update([
-                    'description' => request('description'),
-                ]);
-            }
-            if($request->has('assigned_to') && $request->assigned_to != ''){
-                $task->update([
-                    'assigned_to' => request('assigned_to'),
-                ]);
-            }
-            return responseJson(1, 'Task updated successfully');
+        if(!$task) {
+            return responseJson(0, 'Task not found');
         }
-        return responseJson(0, 'Task not found');
+        $project = $task->project;
+        if(!$user || !$user->is_admin || $user->id != $project->created_by) {
+            return responseJson(0, 'User not authorized');
+        }
+        if($task->submitted){
+            return responseJson(0, 'Can not update submitted task');
+        }
+        if($request->has('title') && $request->title != ''){
+            $task->update([
+                'title' => request('title'),
+            ]);
+        }
+        if($request->has('description') && $request->description != ''){
+            $task->update([
+                'description' => request('description'),
+            ]);
+        }
+        if($request->has('assigned_to') && $request->assigned_to != ''){
+            $task->update([
+                'assigned_to' => request('assigned_to'),
+            ]);
+        }
+        return responseJson(1, 'Task updated successfully');
+    }
+
+    public function submit($id) {
+        $user = auth()->user();
+        $task = Task::find($id);
+        if($user && $task && !$user->is_admin && $user->id == $task->assigned_to && $task->submitted == 0) {
+            $task->update([
+                'submitted' => 1,
+            ]);
+            return responseJson(1, 'Task submitted succesfully');
+        }
+        return responseJson(0, 'User not authorized');
     }
 
     public function delete($id)
@@ -85,7 +109,7 @@ class TaskController extends Controller
         }
         $project = $task->project;
         $user = auth()->user();
-        if ($task && $user && $user->is_admin && $user->id == $project->created_by){
+        if ($user && $user->is_admin && $user->id == $project->created_by){
             $task->delete();
             return responseJson(1, 'Task deleted successfully');
         }
